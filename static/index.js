1eb87eed3d7237a8d204c32df0e0942982c98bff
Manuel <lagosmanual@gmail.com>
2021-01-20 21:53:50 -0300

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1,9 +1,32 @@
 const textbox = document.getElementById("textbox");
 const result = document.getElementById("result");
+const hiden = document.getElementById("hiden");
+const copy = document.getElementById("copy");
+const copied = document.getElementById("copied");
 
 textbox.addEventListener("keyup", translate);
 textbox.addEventListener("change", translate);
+copy.addEventListener("click", copytext)
 
 function translate() {
-  result.innerHTML = textbox.value.replace(/[aeiuoáéíóú]/g, "i").replace(/[AEIOUÁÉÍÓÚ]/g, "I");
+  const out = textbox.value.replace(/[aeiuoáéíóú]/g, "i").replace(/[AEIOUÁÉÍÓÚ]/g, "I");
+  result.innerHTML = out;
+  hiden.value = out;
+}
+
+function copytext() {
+  hiden.select();
+  hiden.setSelectionRange(0, 99999);
+
+  document.execCommand("copy");
+
+  transition();
+}
+
+function transition() {
+  copied.classList.add("copied-on");
+
+  setTimeout(() => {
+    copied.classList.remove("copied-on");
+  }, 1000);
 }
